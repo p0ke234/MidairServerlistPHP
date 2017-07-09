3eff5895bcc79166b8621c2d49ce12e820938a93
p0ke <p0ke@gmx.de>
2017-07-09 10:13:04 +0200

    Added Midair Serverlist PHP

---
+++ README.md
@@ -0,0 +1,9 @@
+# Midair Serverlist PHP
+
+Shows any Midair server with present players >= 1
+
+Supported platforms
+--------------------
+* Tested with PHP 5.6.30 / Apache (Windows 10)
+
+![alt example](https://github.com/p0ke234/MidairServerlistPHP/blob/master/example.png)
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$json = file_get_contents('https://api.bittah.com/servers');
+$array = json_decode($json, true);
+
+echo "<html>";
+echo "<LINK REL='StyleSheet' HREF='style.css' TYPE='text/css'>";
+echo "<center>";
+echo "<table class=\"Server\" border=1 cellpadding=5 cellspacing=0>";
+echo "<tr><th colspan=3 align=center>MIDAIR Serverlist</th></tr>";
+echo "<tr><th align=left>Name</th><th align=left>Map</th><th align=left>Players</th></tr>";
+foreach($array as $value) { 
+    if ($value['players'] >= 1)
+    {
+    	echo "<tr>";
+    	echo " <td align=left>".$value['name']."</td>";
+    	echo " <td width=300 align=left>".$value['map']."</td>";
+    	echo " <td width=100 align=left>".$value['players']." / ".$value['max_players']."</td>";
+    	echo "</tr>";
+	}
+}
+echo "</center>";
+echo "</table>";
+echo "</html>";
+?>
